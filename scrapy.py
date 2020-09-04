c81784527fe5f54fead4efb9c138678bf910b49b
jayne <jaynetzt12@gmail.com>
2020-09-04 20:51:03 +0800

    v1

--- scrapy.py
+++ scrapy.py
@@ -10,6 +10,13 @@ class NewSpider(scrapy.Spider):
                                 'Image Link': x.xpath(newsel).extract_first(),
                             }
 
+     def parse(self, response):
+                css_selector = 'img'
+                for x in response.css(css_selector):
+                            newsel = '@src'
+                            yield {
+                                'Image Link': x.xpath(newsel).extract_first(),
+
                 Page_selector = '.next a ::attr(href)'
                 next_page = response.css(Page_selector).extract_first()
                 if next_page:

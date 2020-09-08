9707623e7cb231d077f87c42ceb0ba38fcdd98b3
jayne <jaynetzt12@gmail.com>
2020-09-08 10:48:56 +0800

    v3

--- unittest.py
+++ unittest.py
@@ -3,7 +3,9 @@ import os
 
 class TestMyProgram(unittest.TestCase):
     def test(self):
+#Output is shown by getting file from system
         os.system("C:\PycharmProjects\DA_Team08\scrapy.py")
+        os.system("C:\PycharmProjects\DA_Team08\recon.py")
 
     if name == 'main':
         unittest.main()
